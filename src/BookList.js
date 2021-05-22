64e28b5cd0b66357f78f1376c9120e4f1b98563e
Lance Johnson <rlancejohnson@gmail.com>
2021-05-21 22:24:11 -0600

    Do not render books without authors or thumbnails

--- src/BookList.js
+++ src/BookList.js
@@ -5,16 +5,20 @@ import Book from './Book.js'
 export default ({ lists, list, books, updateBook }) => {
   return (
     <div className="bookshelf">
-      {list && 
+      {list &&
         <h2 className="bookshelf-title">{list.title}</h2>
       }
       <div className="bookshelf-books">
         <ol className="books-grid">
           {books && books.map((book) => (
-            <Book key={book.id} lists={lists} book={book} updateBook={updateBook}/>
+            book.authors &&
+            book.imageLinks &&
+            book.imageLinks.smallThumbnail &&
+
+            <Book key={book.id} lists={lists} book={book} updateBook={updateBook} />
           ))}
         </ol>
       </div>
-    </div>   
+    </div>
   )
 }
