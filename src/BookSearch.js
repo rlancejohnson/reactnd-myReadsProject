a88634f7dde699455b8878a1b7b2bdb5c735b43d
Lance Johnson <rlancejohnson@gmail.com>
2021-05-21 21:45:56 -0600

    Initial build of book search cmp

---
+++ src/BookSearch.js
@@ -0,0 +1,38 @@
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+//import PropTypes from 'prop-types';
+import BookList from './BookList.js';
+
+export default class BookSearch extends Component {
+  componentDidMount = () => {
+    this.props.resetSearch();
+  }
+
+  handleSearch = (e) => {
+    if(e.target.value) {
+      this.props.searchBooks(e.target.value);
+    } else {
+      this.props.resetSearch();
+    }
+  }
+
+  render() {
+    const { lists, searchTerm, searchResults, updateBook } = this.props;
+
+    return (
+      <div className="search-books">
+        <div className="search-books-bar">
+          <Link to='/' className="close-search">Close</Link>
+          <div className="search-books-input-wrapper">
+            <input type="text" placeholder="Search by title or author" value={searchTerm} onChange={this.handleSearch}/>
+          </div>
+        </div>
+        <div className="search-books-results">
+          {searchResults && 
+            <BookList lists={lists} books={searchResults} updateBook={updateBook}/>
+          }
+        </div>
+      </div>
+    )
+  }
+}
