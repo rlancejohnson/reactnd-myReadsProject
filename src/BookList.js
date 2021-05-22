fe97a36c6f1a0814b267c3b3ecb75e924eae2295
Lance Johnson <rlancejohnson@gmail.com>
2021-05-21 21:44:43 -0600

    Initial build of book list cmp

---
+++ src/BookList.js
@@ -0,0 +1,20 @@
+import React from 'react'
+//import PropTypes from 'prop-types'
+import Book from './Book.js'
+
+export default ({ lists, list, books, updateBook }) => {
+  return (
+    <div className="bookshelf">
+      {list && 
+        <h2 className="bookshelf-title">{list.title}</h2>
+      }
+      <div className="bookshelf-books">
+        <ol className="books-grid">
+          {books && books.map((book) => (
+            <Book key={book.id} lists={lists} book={book} updateBook={updateBook}/>
+          ))}
+        </ol>
+      </div>
+    </div>   
+  )
+}
