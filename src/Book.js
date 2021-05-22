f44e1a45265b71c1646f298e90ff6cf66b46c106
Lance Johnson <rlancejohnson@gmail.com>
2021-05-21 22:14:06 -0600

    Add authors to under the title of each book

--- src/Book.js
+++ src/Book.js
@@ -8,18 +8,20 @@ export default ({ lists, book, updateBook }) => {
         <div className="book-top">
           <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url("${book.imageLinks.smallThumbnail}")` }}></div>
           <div className="book-shelf-changer">
-              <select onChange={(event) => updateBook(book, event.target.value)} value={book.shelf}>
-                <option value="move" disabled>Move to...</option>
-                <option value="none">None</option>
-                {lists.map((list) => (
-                  <option key={list.id} value={list.name}>{list.title}</option>
-                ))}
-                
-              </select>
+            <select onChange={(event) => updateBook(book, event.target.value)} value={book.shelf}>
+              <option value="move" disabled>Move to...</option>
+              <option value="none">None</option>
+              {lists.map((list) => (
+                <option key={list.id} value={list.name}>{list.title}</option>
+              ))}
+
+            </select>
           </div>
         </div>
         <div className="book-title">{book.title}</div>
-        <div className="book-authors">{book.author}</div>
+        <div className="book-authors">{book.authors.map(author => (
+          <div>{author}</div>
+        ))}</div>
       </div>
     </li>
   )
