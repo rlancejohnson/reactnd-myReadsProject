57f31727f9cfcee17d529e6465270f9ce188dfe0
Lance Johnson <rlancejohnson@gmail.com>
2021-05-21 21:45:13 -0600

    Initial build of book home cmp

---
+++ src/BookHome.js
@@ -0,0 +1,28 @@
+import React from 'react'
+//import PropTypes from 'prop-types'
+import { Link } from 'react-router-dom'
+import BookList from './BookList.js'
+
+export default ({ appName, lists, books, updateBook }) => {
+  return (
+    <div className="list-books">
+      <div className="list-books-title">
+        <h1>{appName}</h1>
+      </div>
+      <div className="list-books-content">
+        <ol>
+          {lists.map((list) => {
+            const listBooks = books.filter((book) => book.shelf === list.name)
+
+            return (
+              <BookList key={list.id} lists={lists} list={list} books={listBooks} updateBook={updateBook}/>
+            )
+          })}
+        </ol>
+      </div>
+      <div className="open-search">
+        <Link to='/search'>Add a book</Link>
+      </div>
+    </div>
+  )
+}
